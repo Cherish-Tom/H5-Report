4ff65a27256499204b09ce8f0c866822033fd210
Cherish-Tom <cherish_stand@sina.com>
2016-12-01 19:22:10 +0800

    delete

--- src/app/components/list/About.js
+++ src/app/components/list/About.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
-=======
-import React, {Component} from 'react';
->>>>>>> 45c2f93e51384d60f5d81e795eb086c5285b8128
 
-
-
-
-<<<<<<< HEAD
 class About extends React.Component {
   constructor(props) {
       super(props);
@@ -19,20 +11,4 @@ class About extends React.Component {
       )
   }
 }
-
-
-
 export default About;
-=======
-
-class About extends Component {
-    render() {
-        return (
-            <div>ssssss</div>
-        )
-    }
-}
-
-
-export default About
->>>>>>> 45c2f93e51384d60f5d81e795eb086c5285b8128
--- src/app/components/list/Setting.js
+++ src/app/components/list/Setting.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Header from '../public/Header';
 import RaisedButton from 'material-ui/RaisedButton';
-<<<<<<< HEAD
 import Template from '../public/template';
-=======
->>>>>>> 45c2f93e51384d60f5d81e795eb086c5285b8128
 class Setting extends React.Component {
     constructor(props,contxt){
         super(props, contxt);
@@ -13,7 +10,6 @@ class Setting extends React.Component {
         const path = this.props.location.pathname;
         return (
             <div className='warp'>
-<<<<<<< HEAD
                   <Header title='设置'/>
                   <div className='container'>
                       <div className='setting'>
@@ -30,33 +26,11 @@ class Setting extends React.Component {
                           style={{height: 45, fontSize: 16}}
                       />
                   </div>
-=======
-                <Header title='设置' path={path}/>
-                <div className='container'>
-                    <div className='setting'>
-                        <a href='/setting/about' >关于我们</a>
-                        <a href='/setting/about' style={{margin: '15px 0'}}>意见反馈</a>
-                        <a href='#' >清除缓存</a>
-                    </div>
-                    <RaisedButton
-                        label='退出登录'
-                        labelStyle={{fontSize: 16, letterSpacing: 1}}
-                        fullWidth={true}
-                        backgroundColor='rgb(94, 149, 201)'
-                        labelColor='#fff'
-                    />
-                </div>
->>>>>>> 45c2f93e51384d60f5d81e795eb086c5285b8128
+
             </div>
         )
     }
 }
-<<<<<<< HEAD
-
-
 export default Template({
     component: Setting
 })
-=======
-export default Setting
->>>>>>> 45c2f93e51384d60f5d81e795eb086c5285b8128
